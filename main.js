f68db4f16b9ba55e2c09d91f368852c46bcbdab5
peterbcarlson41 <75817779+peterbcarlson41@users.noreply.github.com>
2023-04-11 16:30:01 -0400

    full template for portfolio created

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import './style.css'
 
@@ -6,9 +7,10 @@ import './style.css'
 const scene = new THREE.Scene()
 
 //create sphere
-const geometry = new THREE.IcosahedronGeometry(3);
+const geometry = new THREE.SphereGeometry( 3, 40, 100 );
 const material = new THREE.MeshStandardMaterial({
   color: '#00ff83',
+  roughness: 0.5,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
@@ -23,9 +25,6 @@ const sizes = {
 const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(10, 10, 10);
 scene.add(light);
-const ambientLight = new THREE.PointLight(0x404040, 1, 100);
-ambientLight.position.set(-10, 10, -20);
-scene.add(ambientLight);
 
 //Camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
@@ -65,3 +64,30 @@ const loop = () => {
 }
 
 loop();
+
+//Timeline
+
+const tl = gsap.timeline({defaults: {duration: 1}})
+tl.fromTo(mesh.scale, {z:0, x:0, y: 0}, {z:1, x:1, y:1})
+tl.fromTo('nav', {y: '-100%'}, {y: "0%"})
+tl.fromTo('.title', {opacity: 0}, {opacity: 1})
+
+//Mouse Animator Color
+let mouseDown = false;
+let rgb = [];
+window.addEventListener('mousedown', () => (mouseDown = true))
+window.addEventListener('mouseup', () => (mouseDown = false))
+
+window.addEventListener('mousemove', (e) => {
+  if(mouseDown){
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ]
+
+    //animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(',')})`)
+    gsap.to(mesh.material.color, {r: newColor.r, g: newColor.g, b: newColor.b})
+  }
+})
